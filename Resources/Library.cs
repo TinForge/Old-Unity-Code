24a37fe51cf28c0a521cef9de477f368291ac695
TinForge <29712552+TinForge@users.noreply.github.com>
2017-11-02 02:01:49 -0400

    Add files via upload

---
+++ Resources/Library.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Tin.Resources
+{
+	public class Library: MonoBehaviour
+	{
+		private static Library instance;
+
+		[SerializeField] private AudioClip clickUI;
+		[SerializeField] private AudioClip failureUI;
+		[SerializeField] private AudioClip successUI;
+
+
+		public static AudioClip ClickUI;
+
+		public static AudioClip FailureUI;
+
+		public static AudioClip SuccessUI;
+
+		void Awake ()
+		{
+			if (instance != this && instance != null)
+				Destroy (gameObject);
+
+			ClickUI = clickUI;
+			FailureUI = FailureUI;
+			SuccessUI = successUI;
+		}
+	}
+}
