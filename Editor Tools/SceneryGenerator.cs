b6c61f3c2549dc3a139fdec1cb6e31af7b1a2035
TinForge <29712552+TinForge@users.noreply.github.com>
2018-09-10 11:57:03 -0400

    Added Scenery Generator

---
+++ Editor Tools/SceneryGenerator.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TinForge.SceneryGenerator
+{
+	[System.Serializable]
+	public class SceneryPrefab
+	{
+		public GameObject prefab;
+
+		public int amount;
+		public bool alignToGround;
+		public bool randomRotation;
+		public float minTilt;
+		public float maxTilt;
+		public bool randomScale;
+		public float minScale;
+		public float maxScale;
+		public float verticalOffset;
+
+		public Vector3 Bounds { get { return prefab.GetComponentInChildren<Renderer>().bounds.size; } }
+		public float Volume { get { return Bounds.x * Bounds.y * Bounds.z; } }
+
+
+		public SceneryPrefab()
+		{
+			prefab = null;
+			minTilt = SceneryGenerator.minPrefabTilt;
+			maxTilt = SceneryGenerator.maxPrefabTilt;
+			minScale = SceneryGenerator.minPrefabScale;
+			maxScale = SceneryGenerator.maxPrefabScale;
+		}
+	}
+
+
+
+
+	public class SceneryGenerator : MonoBehaviour
+	{
+
+		//Min Max Values
+		public const int minPrefabTilt = 0;
+		public const int maxPrefabTilt = 90;
+		public const float minPrefabScale =1f;
+		public const float maxPrefabScale = 5f;
+		public const float maxPrefabVertical = 2f;
+
+		//Random Placement Configurations
+		public bool raycastAll;
+		public Transform terrain;
+		public bool sortSize;
+		public bool overwrite;
+		public bool autogen;
+
+		public bool manualSeed;
+		public int randomSeed;
+
+		//public int maxTangent;
+		public int raycastHeight;
+		public int radius;
+
+		//Prefabs and properties
+		public int prefabCount;
+		public SceneryPrefab[] prefabs;
+
+
+		//-----------------------
+
+		public void Generate()
+		{
+			if (overwrite || autogen)
+				RemoveAll();
+
+				if (sortSize)
+					SortSizePrefabs();
+
+			for (int i = 0; i < prefabs.Length; i++)
+			{
+				if (manualSeed)
+					Random.InitState(randomSeed*(i+1));
+
+				for (int j = 0; j < prefabs[i].amount; j++)
+				{
+					Vector3 pos;
+					Quaternion rot;
+					RaycastPlacement(prefabs[i].alignToGround, out pos, out rot);
+					if (pos == Vector3.zero)
+						continue;
+
+					Transform obj = PlaceScenery(prefabs[i], pos, rot);
+					if (prefabs[i].randomRotation)
+						RandomRotation(obj);
+					if (prefabs[i].randomRotation)
+						RandomTilt(obj, prefabs[i].minTilt, prefabs[i].maxTilt);
+					if (prefabs[i].randomScale)
+						RandomScale(obj, prefabs[i].minScale, prefabs[i].maxScale, prefabs[i].verticalOffset);
+				}
+			}
+
+		}
+
+		public void RemoveAll()
+		{
+			for (int i = transform.childCount - 1; i >= 0; i--)
+				DestroyImmediate(transform.GetChild(i).gameObject);
+		}
+
+		public void Remove(Transform group)
+		{
+			DestroyImmediate(group.gameObject);
+		}
+
+		public void SortSizePrefabs()
+		{
+			for (int i = 0; i < prefabs.Length; i++)
+				for (int j = 0; j < prefabs.Length; j++)
+				{
+					if (prefabs[i].Volume < prefabs[j].Volume)
+					{
+						SceneryPrefab temp = prefabs[i];
+						prefabs[i] = prefabs[j];
+						prefabs[j] = temp;
+					}
+				}
+		}
+
+		public void RaycastPlacement(bool align, out Vector3 pos, out Quaternion rot)
+		{
+			Vector3 origin = transform.position + Vector3.up * raycastHeight;
+			origin = origin + new Vector3(Random.Range(-radius, radius), 0, Random.Range(-radius, radius));
+
+			pos = Vector3.zero;
+			rot = Quaternion.identity;
+			RaycastHit hit;
+
+			if (Physics.Raycast(new Ray(origin, Vector3.down), out hit, raycastHeight))
+			{
+				if (!raycastAll && hit.transform != terrain)
+					return;
+
+				pos = hit.point;
+				if (align)
+					rot = Quaternion.FromToRotation(Vector3.up, hit.normal);
+			}
+		}
+
+		public Transform PlaceScenery(SceneryPrefab obj, Vector3 pos, Quaternion rot)
+		{
+			Transform parent = transform.Find(obj.prefab.name);
+			if (parent == null)
+			{
+				parent = new GameObject().transform;
+				parent.name = obj.prefab.name;
+				parent.transform.parent = transform;
+			}
+			return Instantiate(obj.prefab, pos, rot, parent).transform;
+		}
+
+		public void RandomRotation(Transform obj)
+		{
+			obj.Rotate(Vector3.up * Random.Range(0, 360));
+		}
+
+		public void RandomTilt(Transform obj, float min, float max)
+		{
+			obj.Rotate(Vector3.forward * Random.Range(min, max));
+		}
+
+		public void RandomScale(Transform obj, float min, float max, float vRange)
+		{
+			float s = Random.Range(min, max);
+			obj.localScale = new Vector3(s, s + Random.Range(0, vRange), s);
+		}
+
+
+	}
+}
---
+++ Editor Tools/SceneryGeneratorEditor.cs
@@ -0,0 +1,251 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace TinForge.SceneryGenerator
+{
+
+	[CustomEditor(typeof(SceneryGenerator))]
+	public class SceneryGeneratorEditor : Editor
+	{
+		SerializedProperty raycastAllProp;
+		SerializedProperty terrainProp;
+		SerializedProperty sortSizeProp;
+		SerializedProperty overwriteProp;
+		SerializedProperty autogenProp;
+
+		SerializedProperty manualSeedProp;
+		SerializedProperty randomSeedProp;
+
+		SerializedProperty raycastHeightProp;
+		SerializedProperty radiusProp;
+
+		SerializedProperty prefabCountProp;
+		SerializedProperty prefabsProp;
+
+		void OnEnable()
+		{
+			raycastAllProp = serializedObject.FindProperty("raycastAll");
+			terrainProp = serializedObject.FindProperty("terrain");
+			sortSizeProp = serializedObject.FindProperty("sortSize");
+			overwriteProp = serializedObject.FindProperty("overwrite");
+			autogenProp = serializedObject.FindProperty("autogen");
+
+			manualSeedProp = serializedObject.FindProperty("manualSeed");
+			randomSeedProp = serializedObject.FindProperty("randomSeed");
+
+			raycastHeightProp = serializedObject.FindProperty("raycastHeight");
+			radiusProp = serializedObject.FindProperty("radius");
+
+			prefabCountProp = serializedObject.FindProperty("prefabCount");
+			prefabsProp = serializedObject.FindProperty("prefabs");
+		}
+
+		private static bool bingbong = true;
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+
+			bingbong = EditorGUILayout.Foldout(bingbong, "Doesgthsfuck");
+			if(bingbong)
+				EditorGUILayout.PropertyField(terrainProp, new GUIContent("Target Mesh"));
+
+
+			#region Placement Settings
+			GUILayout.Label(new GUIContent("Placement Settings"), EditorStyles.boldLabel);
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+			EditorGUILayout.PropertyField(raycastAllProp, new GUIContent("Place Anywhere", "Scenery prefabs will be generated on any raycasted surface. Recommend using a target mesh instead."));
+			EditorGUI.indentLevel = 1;
+			EditorGUILayout.BeginHorizontal();
+			if (!raycastAllProp.boolValue)EditorGUI.indentLevel = 1;
+				EditorGUILayout.PropertyField(terrainProp, new GUIContent("Target Mesh"));
+			EditorGUI.indentLevel = 0;
+			EditorGUILayout.EndHorizontal();
+			EditorGUILayout.PropertyField(raycastHeightProp, new GUIContent("Raycast Height","Height should be higher than tallest point of terrain mesh"));
+			EditorGUILayout.PropertyField(radiusProp,new GUIContent("Radius"));
+			//EditorGUILayout.IntSlider(raycastHeightProp, 1, 25, new GUIContent("Raycast Height","Height should be higher than tallest point of terrain mesh"));
+			//EditorGUILayout.IntSlider(radiusProp, 1, 100, new GUIContent("Radius"));
+			GUILayout.Label(new GUIContent("Position Scenery Generator under target mesh. Red plane should not be visible."), EditorStyles.miniLabel);
+			EditorGUILayout.EndVertical();
+			#endregion
+
+			EditorGUILayout.Space();
+
+			#region Prefab Panel
+			GUILayout.Label(new GUIContent("Prefabs Panel"), EditorStyles.boldLabel);
+			for (int i = 0; i < prefabCountProp.intValue; i++)
+			{
+				EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+				EditorGUILayout.BeginHorizontal();
+
+				EditorGUILayout.LabelField("Scenery Prefab " + (i + 1),EditorStyles.boldLabel);
+				if (GUILayout.Button("Delete Prefab"))
+				{
+					prefabCountProp.intValue--;
+					prefabsProp.DeleteArrayElementAtIndex(i);
+				}
+				EditorGUILayout.EndHorizontal();
+				EditorGUILayout.Space();
+				EditorGUI.indentLevel = 1;
+
+				//there seems to be a problem with gui.changed recognizing properties in array?
+
+
+				EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("prefab"));
+				EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("amount"));
+				EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("alignToGround"));
+				EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("randomRotation"));
+				if (prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("randomRotation").boolValue)
+				{
+					EditorGUILayout.BeginHorizontal();
+					EditorGUI.indentLevel = 2;
+					float minTilt = prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("minTilt").floatValue;
+					float maxTilt = prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("maxTilt").floatValue;
+					EditorGUILayout.PrefixLabel("Min " + (int) minTilt + " Max " + (int) maxTilt);
+					EditorGUI.indentLevel = 0;
+					EditorGUILayout.MinMaxSlider(ref minTilt, ref maxTilt, SceneryGenerator.minPrefabTilt, SceneryGenerator.maxPrefabTilt);
+					EditorGUI.indentLevel = 1;
+					prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("minTilt").floatValue = minTilt;
+					prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("maxTilt").floatValue = maxTilt;
+					EditorGUILayout.EndHorizontal();
+				}
+				EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("randomScale"));
+				if (prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("randomScale").boolValue)
+				{
+					EditorGUILayout.BeginHorizontal();
+					EditorGUI.indentLevel = 2;
+					float minScale = prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("minScale").floatValue;
+					float maxScale = prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("maxScale").floatValue;
+					EditorGUILayout.PrefixLabel("Min " +  minScale.ToString("0.0") + " Max " + maxScale.ToString("0.0"));
+					EditorGUI.indentLevel = 0;
+					EditorGUILayout.MinMaxSlider(ref minScale, ref maxScale, SceneryGenerator.minPrefabScale,SceneryGenerator.maxPrefabScale);
+					EditorGUI.indentLevel = 1;
+					prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("minScale").floatValue = minScale;
+					prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("maxScale").floatValue = maxScale;
+					EditorGUILayout.EndHorizontal();
+					EditorGUI.indentLevel = 2;
+					EditorGUILayout.Slider(prefabsProp.GetArrayElementAtIndex(i).FindPropertyRelative("verticalOffset"), 0, SceneryGenerator.maxPrefabVertical);
+				}
+				EditorGUI.indentLevel = 0;
+				EditorGUILayout.EndVertical();
+				
+
+				EditorGUILayout.Space();
+			}
+
+			if (GUILayout.Button("Add Prefab"))
+			{
+				prefabCountProp.intValue++;
+				prefabsProp.arraySize++;
+			}
+
+			EditorGUILayout.PropertyField(prefabsProp.GetArrayElementAtIndex(0));
+			EditorGUILayout.LabelField(prefabsProp.GetArrayElementAtIndex(0).propertyPath);
+			#endregion
+
+			EditorGUILayout.Space();
+
+			#region Random Settings
+			GUILayout.Label(new GUIContent("Random Settings"), EditorStyles.boldLabel);
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+			EditorGUILayout.PropertyField(manualSeedProp, new GUIContent("Use manual seed"));
+			if (manualSeedProp.boolValue) {
+				EditorGUI.indentLevel = 1;
+				EditorGUILayout.BeginHorizontal();
+				EditorGUILayout.PropertyField(randomSeedProp);
+				if (GUILayout.Button("Randomise Seed"))
+					randomSeedProp.intValue = Random.Range(0, 9999);
+				EditorGUI.indentLevel = 0;
+				EditorGUILayout.EndHorizontal();
+			}
+			EditorGUILayout.EndVertical();
+
+			#endregion
+
+			EditorGUILayout.Space();
+
+			#region Control Panel
+			GUILayout.Label(new GUIContent("Control Panel"), EditorStyles.boldLabel);
+
+			EditorGUILayout.Space();
+			EditorGUILayout.BeginHorizontal();
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+			EditorGUILayout.PropertyField(sortSizeProp, new GUIContent("Sort prefabs by size"));
+			//EditorGUILayout.PropertyField(overwriteProp, new GUIContent("Overwrite generated"));
+			EditorGUILayout.PropertyField(autogenProp, new GUIContent("Auto generate"));
+
+			EditorGUILayout.EndVertical();
+			#endregion
+
+			EditorGUILayout.Space();
+
+			#region Buttons
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+			EditorGUI.BeginDisabledGroup(autogenProp.boolValue);
+			if (GUILayout.Button("Generate"))
+				Selection.activeTransform.GetComponent<SceneryGenerator>().Generate();
+
+			if (GUILayout.Button("Remove All"))
+				Selection.activeTransform.GetComponent<SceneryGenerator>().RemoveAll();
+			EditorGUI.EndDisabledGroup();
+
+			EditorGUILayout.EndVertical();
+			EditorGUILayout.EndHorizontal();
+
+			#endregion
+
+			EditorGUILayout.Space();
+
+			#region Info
+
+			GUILayout.Label(new GUIContent("About"), EditorStyles.boldLabel);
+			EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+			GUILayout.Label(new GUIContent("A useful tool for quickly generating randomised scenery during editing. Put in prefabs, adjust settings, and generate! Uses raycasting and random range for placement. Childs and sorts objects by prefabs."), EditorStyles.wordWrappedLabel);
+
+			GUILayout.Label(new GUIContent("*Detach children from Scenery Generator when happy with results"), EditorStyles.wordWrappedMiniLabel);
+			GUILayout.Label(new GUIContent("*Prefabs get generated in numerical order. Largest object should be placed first. Ex. grass should go last."), EditorStyles.wordWrappedMiniLabel);
+			GUILayout.Label(new GUIContent("*Clipping is still possible as raycast do not check for thickness."), EditorStyles.wordWrappedMiniLabel);
+
+			GUILayout.Label(new GUIContent("v1.0 By TinForge 2018"), EditorStyles.centeredGreyMiniLabel);
+			EditorGUILayout.EndVertical();
+			#endregion
+
+			EditorGUILayout.Space();
+
+			if (GUI.changed)
+				if (autogenProp.boolValue && overwriteProp.boolValue)
+					Selection.activeTransform.GetComponent<SceneryGenerator>().Generate();
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		public void OnSceneGUI()
+		{
+			SceneryGenerator sg = target as SceneryGenerator;
+			Handles.color = Color.blue;
+			Handles.zTest = UnityEngine.Rendering.CompareFunction.Always;
+			Handles.DrawLine(sg.transform.position, sg.transform.position + Vector3.up * raycastHeightProp.intValue);
+
+			Color red = Color.red;
+			red.a = 0.35f;
+			Handles.color = red;
+			Handles.zTest = UnityEngine.Rendering.CompareFunction.LessEqual;
+			Vector3[] verts = new[] {
+				new Vector3 (sg.transform.position.x - radiusProp.intValue, sg.transform.position.y, sg.transform.position.z - radiusProp.intValue),
+				new Vector3(sg.transform.position.x - radiusProp.intValue, sg.transform.position.y, sg.transform.position.z + radiusProp.intValue),
+				new Vector3(sg.transform.position.x + radiusProp.intValue, sg.transform.position.y, sg.transform.position.z + radiusProp.intValue),
+				new Vector3(sg.transform.position.x + radiusProp.intValue, sg.transform.position.y, sg.transform.position.z - radiusProp.intValue)};
+			Handles.DrawSolidRectangleWithOutline(verts, red, red);
+
+		}
+
+
+	}
+}
